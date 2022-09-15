06b3c83d23e2d99475f56d91fae0d9216daa9899
Yoga Pratama <yogapratama7212@gmail.com>
2022-09-15 21:45:03 +0700

    FEAT: 3rd day task

---
+++ day-3/config/config.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"alterra-agmc/day-3/models"
+	"fmt"
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var (
+	db  *gorm.DB
+	err error
+)
+
+func Connect() *gorm.DB {
+	username := os.Getenv("DB_USERNAME")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	dbhost := os.Getenv("DB_HOST")
+	dbport := os.Getenv("DB_PORT")
+
+	dns := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", username, password, dbhost, dbport, dbname)
+
+	db, err = gorm.Open(mysql.Open(dns), &gorm.Config{})
+	if err != nil {
+		panic("Cannot connect database")
+	}
+
+	MigrateDB()
+
+	return db
+}
+
+func MigrateDB() {
+	db.AutoMigrate(&models.User{})
+	db.AutoMigrate(&models.Book{})
+}
---
+++ day-3/routes/routes.go
@@ -0,0 +1,65 @@
+package routes
+
+import (
+	"alterra-agmc/day-3/config"
+	"alterra-agmc/day-3/controllers"
+	"alterra-agmc/day-3/handlers"
+	"alterra-agmc/day-3/lib/database"
+	"alterra-agmc/day-3/middlewares"
+	"os"
+
+	"github.com/go-playground/validator"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func Init() *echo.Echo {
+	e := echo.New()
+
+	// Using custom validation
+	e.Validator = &handlers.CustomValidation{Validator: validator.New()}
+
+	// Hide Echo Banner
+	e.HideBanner = true
+
+	// Middlewares
+	middlewares.LoggerMiddleware(e)
+	middlewares.RecoverMiddleware(e)
+	middlewares.CorsMiddleware(e)
+
+	connection := config.Connect()
+	userRepository := database.NewUserRepository(connection)
+	userController := controllers.Init(userRepository)
+
+	bookRepository := database.NewBookRepository(connection)
+	bookController := controllers.InitBook(bookRepository)
+
+	v1 := e.Group("/api/v1")
+
+	v1.POST("/users/", userController.CreateUserControllers)
+	user := v1.Group("/users")
+	user.Use(middleware.JWTWithConfig(middleware.JWTConfig{
+		SigningMethod: "HS512",
+		SigningKey:    []byte(os.Getenv("SECRET_KEY")),
+		TokenLookup:   "cookie:token",
+	}))
+	user.GET("", userController.GetUsersControllers)
+	user.GET("/:id", userController.GetUserByIdControllers)
+	user.PUT("/:id", userController.UpdateUserControllers)
+	user.DELETE("/:id", userController.DeleteUserControllers)
+
+	// BOOK
+	v1.GET("/books/", bookController.GetBooksControllers)
+	v1.GET("/books/:id", bookController.GetBookByIdControllers)
+
+	book := v1.Group("/books")
+	book.Use(middleware.JWTWithConfig(middleware.JWTConfig{
+		SigningMethod: "HS512",
+		SigningKey:    []byte(os.Getenv("SECRET_KEY")),
+		TokenLookup:   "cookie:token",
+	}))
+	book.POST("", bookController.CreateBookControllers)
+	book.PUT("/:id", bookController.UpdateBookControllers)
+	book.DELETE("/:id", bookController.DeleteBookControllers)
+	return e
+}
